343a5aba6f5cf224a0fd568750323705f7ee6c8f
mariam <mariamtobasy04@gmail.com>
2025-10-16 15:29:10 +0300

    Add backend for Devo project

---
+++ DevoBackend/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using DevoBackend.Models; // <-- where all your model classes are
+
+public class User
+{
+    public int UserId { get; set; }
+    public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public string? JobTitle { get; set; }
+    public int? DepartmentId { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Location { get; set; }
+    public string? Role { get; set; }
+    public string? ProfilePhoto { get; set; }
+
+    // Navigation properties
+    public ICollection<Team> CreatedTeams { get; set; } = new List<Team>();
+    public ICollection<TeamMember> TeamMemberships { get; set; } = new List<TeamMember>();
+    public ICollection<AssignedTask> AssignedTasks { get; set; } = new List<AssignedTask>();
+    public ICollection<AssignedTask> CreatedTasks { get; set; } = new List<AssignedTask>();
+    public ICollection<TaskComment> TaskComments { get; set; } = new List<TaskComment>();
+    public ICollection<TaskHistory> TaskHistories { get; set; } = new List<TaskHistory>();
+    public ICollection<Meeting> CreatedMeetings { get; set; } = new List<Meeting>();
+    public ICollection<MeetingParticipant> MeetingParticipations { get; set; } = new List<MeetingParticipant>();
+    public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+    public ICollection<HelpArticle> HelpArticles { get; set; } = new List<HelpArticle>();
+    public ICollection<ContactMessage> ContactMessages { get; set; } = new List<ContactMessage>();
+    public ICollection<Note> Notes { get; set; } = new List<Note>();
+    public ICollection<UserActivity> UserActivities { get; set; } = new List<UserActivity>();
+}
+
